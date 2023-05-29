b522150c24106e9ebcbb94840d43a7975cb4042a
Mustafa Yilmaz <96684729+lazymustafa@users.noreply.github.com>
2023-05-29 15:04:52 +0300

    Update Ghost.cpp

--- Ghost.cpp
+++ Ghost.cpp
@@ -1,15 +1,15 @@
 #include "Ghost.h"
 #include "qpen.h"
+#include "Player.h"
 
 
-
-
-Ghost::Ghost(int sceneLengthY , int sceneWidthX ){
+Ghost::Ghost(int sceneLengthY , int sceneWidthX, Player &player ){
 
     setRect(10,10,10,10);
     setPen(QPen(QColor(255, 0, 0), 2));
     setBrush(QBrush(QColor(15, 144, 2)));
     this->isActive = true;
+    this->player = &player;
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -40,14 +40,33 @@ void Ghost::setIsActive(bool control){
     this->isActive = control;
 }
 
+bool Ghost::active(){
+    bool isActive = true;
+
+//hayaletin durma algoritmasi 
+    if (player->getPosX() == this->getPosX()
+        ||  player->getPosY() == this->getPosY())
+    {
+        isActive = false;
+    }
+
+    return isActive;
+}
+
 
 void Ghost::randomMove(){
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> distribution(0, 3);
 
+    bool boolen = true ;
 
-    while(this-isActive){
+    while(boolen){
+
+        if(!this->active()){
+            boolen = false;
+        }
+
         int randomNumber = distribution(gen);
 
         switch(randomNumber)
