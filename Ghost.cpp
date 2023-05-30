3cc601e456b9ef124f2ae41bb49d7d3dd1c66fb2
ErvaYlmz <130571315+ErvaYlmz@users.noreply.github.com>
2023-05-30 23:26:28 +0300

    Update Ghost.cpp

--- Ghost.cpp
+++ Ghost.cpp
@@ -3,6 +3,7 @@
 #include "Player.h"
 #include <random>
 #include <QTimer>
+
 Ghost::Ghost(int sceneLengthY , int sceneWidthX, Player &player1, Player &player2 ){
     setPixmap(QPixmap(":/images/ghost.png"));
     this->isActive = true;
@@ -49,67 +50,66 @@ bool Ghost::active(){
     bool isActive = true;
 
     //hayaletin durma algoritmasi
-    if (player->getPosX() - 50 < this->getPosX()
-        &&  player->getPosY() + 114 > this->getPosY()
-        && player->getPosX() + 164 > this->getPosX()
-        &&  player->getPosY() -50 < this->getPosY() )
+    if (player1->getPosX() - 50 < this->getPosX()
+        &&  player1->getPosY() + 114 > this->getPosY()
+        && player1->getPosX() + 164 > this->getPosX()
+        &&  player1->getPosY() -50 < this->getPosY())
     {
         setVisible(true);
         isActive = false;
     }else
-         setVisible(false);
+        setVisible(false);
 
-    if (player->getPosX() + 30 < this->getPosX()
-        &&  player->getPosY() + 64 > this->getPosY()
-        && player->getPosX() + 84 > this->getPosX()
-        &&  player->getPosY()  < this->getPosY() )
+    if (player1->getPosX() + 30 < this->getPosX()
+        &&  player1->getPosY() + 64 > this->getPosY()
+        && player1->getPosX() + 84 > this->getPosX()
+        &&  player1->getPosY()  < this->getPosY() )
     {
         timer->stop();
         int point ;
-        point = player->getScore();
+        point = player1->getScore();
         point = point+1;
-        player->setScore(point);
+        player1->setScore(point);
     }
-
 
     return isActive;
 }
 
 
 void Ghost::randomMove(){
-   std::random_device rd;
+    std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> distribution(0, 3);
 
     this->active();
 
-        int randomNumber = distribution(gen);
+    int randomNumber = distribution(gen);
 
-        switch(randomNumber)
-        {
-        case 0:
-            if (x()<=20 || y()<=20 )
-                break;
-            setPos(x()-10,y()-10);
+    switch(randomNumber)
+    {
+    case 0:
+        if (x()<=20 || y()<=20 )
             break;
-        case 1:
-            if (x()>=sceneWidth  || y()<=20 )
-                break;
-            setPos(x()+10,y()-10);
+        setPos(x()-10,y()-10);
+        break;
+    case 1:
+        if (x()>=sceneWidth  || y()<=20 )
             break;
-        case 2:
-            if (x()>=sceneWidth  || y() >= sceneLength )
-                break;
-            setPos(x()+10,y()+10);
+        setPos(x()+10,y()-10);
+        break;
+    case 2:
+        if (x()>=sceneWidth  || y() >= sceneLength )
             break;
-        case 3:
-            if (x()<=20 || y() >= sceneLength )
-                break;
-            setPos(x()-10,y()+10);
+        setPos(x()+10,y()+10);
+        break;
+    case 3:
+        if (x()<=20 || y() >= sceneLength )
             break;
+        setPos(x()-10,y()+10);
+        break;
 
         posX = x();
         posY = y();
 
 
-        }}
+    }}
