1a0ab912010920376faebce0e72a2253af3f8ef3
Sumeyye05 <130572281+Sumeyye05@users.noreply.github.com>
2023-05-30 13:38:22 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,86 +1,106 @@
+// Player.cpp
+
 #include "Player.h"
-#include "qbrush.h"
-#include "qevent.h"
-#include <iostream>
-#include <random>
+#include <QKeyEvent>
 
-Player::Player(int sceneLengthY , int sceneWidthX, int l=100, int w=100, int posX = 0, int posY =0 ){
-
-    //setBrush(QBrush(QColor(255, 255, 255)));
-    setPixmap(QPixmap(":/images/startIcon.png"));
-    length = l;
-    width = w;
-    posX = posX;
-    posY = posY;
-    sceneLength = sceneLengthY ;
-    sceneWidth = sceneWidthX ;
-    //setRect(0,0,l,w);
-    setPos(posX,posY);
-    setFlag(QGraphicsItem::ItemIsFocusable);
-    setFocus();
+Player::Player(int l, int w, int posX, int posY, int sceneLength, int sceneWidth)
+    : length(l), width(w), posX(posX), posY(posY), sceneLength(sceneLength), sceneWidth(sceneWidth), score(0)
+{
 }
 
-
-int Player::getLength() {
+int Player::getLength()
+{
     return length;
 }
 
-int Player::getWidth() {
+int Player::getWidth()
+{
     return width;
 }
 
-int Player::getArea() {
+int Player::getArea()
+{
     return length * width;
 }
 
-int Player::getPosX() {
+int Player::getPosX()
+{
     return posX;
 }
 
-int Player::getPosY() {
+int Player::getPosY()
+{
     return posY;
 }
 
-int Player::getSceneLength() {
+int Player::getSceneLength()
+{
     return sceneLength;
 }
 
-int Player::getSceneWidth() {
+int Player::getSceneWidth()
+{
     return sceneWidth;
 }
 
-int Player::getScore(){
+int Player::getScore()
+{
     return score;
 }
-void Player::setScore(int point){
-    this->score = point;
+
+void Player::setScore(int point)
+{
+    score = point;
 }
 
-
-void Player::keyPressEvent(QKeyEvent *event){
-    if (event->key() == Qt::Key_Left){
-        setPos(x()-10,y());
+void Player::moveLeft()
+{
+    if (posX > 0) {
+        setPos(x() - 10, y());
+        posX -= 10;
     }
-    else if (event->key() == Qt::Key_Right ){
-        setPos(x()+10,y());
-    }
-    else if (event->key() == Qt::Key_Up ){
-        setPos(x(),y()-10);
-    }
-    else if (event->key() == Qt::Key_Down ){
-        setPos(x(),y()+10);
-    }
+}
 
-    if( x()<= 0)
-        setPos(sceneWidth + x(),y());
-    if( x()>= sceneWidth)
-        setPos(x() - sceneWidth ,y());
-    if( y()<=0)
-        setPos(x(),y() + sceneLength);
-    if( y() >= sceneLength)
-        setPos(x(),y() - sceneLength );
+void Player::moveRight()
+{
+    if (posX + length < sceneLength) {
+        setPos(x() + 10, y());
+        posX += 10;
+    }
+}
 
+void Player::moveUp()
+{
+    if (posY > 0) {
+        setPos(x(), y() - 10);
+        posY -= 10;
+    }
+}
 
-    posX = x();
-    posY = y();
+void Player::moveDown()
+{
+    if (posY + width < sceneWidth) {
+        setPos(x(), y() + 10);
+        posY += 10;
+    }
+}
+
+void Player::keyPressEvent(QKeyEvent* event)
+{
+    switch (event->key()) {
+    case Qt::Key_Left:
+        moveLeft();
+        break;
+    case Qt::Key_Right:
+        moveRight();
+        break;
+    case Qt::Key_Up:
+        moveUp();
+        break;
+    case Qt::Key_Down:
+        moveDown();
+        break;
+    default:
+        break;
+    }
 }
