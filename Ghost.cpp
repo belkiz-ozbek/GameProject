6f58ddf06e6a1752033e556dceed406c7ecf198a
Beyza Karakurt <125734176+KarakurtBeyza@users.noreply.github.com>
2023-05-30 15:03:55 +0300

    Update Ghost.cpp

--- Ghost.cpp
+++ Ghost.cpp
@@ -1,23 +1,63 @@
-// Ghost.cpp
-
 #include "Ghost.h"
-#include <QTimer>
-#include <QGraphicsScene>
-#include <QList>
-#include <stdlib.h>
+#include "qpen.h"
 #include "Player.h"
+#include <random>
+#include <QTimer>
+Ghost::Ghost(int sceneLengthY , int sceneWidthX, Player &player ){
+    setPixmap(QPixmap(":/images/ghost.png"));
+    this->isActive = true;
+    this->sceneLength =sceneLengthY;
+    this->sceneWidth = sceneWidthX;
+    this->player = &player;
 
-Ghost::Ghost(int sceneLength, int sceneWidth, Player& player)
-    : sceneLength(sceneLength), sceneWidth(sceneWidth), player(player)
-{
-    setRect(0, 0, 20, 20);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> distribution(0, sceneWidthX);
+    int posX = distribution(gen);
+    std::uniform_int_distribution<int> dis(0, sceneLengthY);
+    int posY = dis(gen);
+    setPos(posX,  posY);
+
     QTimer* timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(randomMove()));
     timer->start(100);
+
+    this->posX = posX;
+    this->posY = posY;
 }
 
-void Ghost::randomMove()
-{
+
+int Ghost::getPosX() {
+    return posX;
+}
+
+int Ghost::getPosY() {
+    return posY;
+}
+
+bool Ghost::getIsActive(){
+    return (bool) this->isActive;
+}
+void Ghost::setIsActive(bool control){
+    this->isActive = control;
+}
+
+bool Ghost::active(){
+    bool isActive = true;
+
+    //hayaletin durma algoritmasi
+    if (player->getPosX() == this->getPosX()
+        ||  player->getPosY() == this->getPosY())
+    {
+        isActive = false;
+    }
+
+    return isActive;
+}
+
+
+void Ghost::randomMove(){
     int randDirection = rand() % 4; // 0: up, 1: right, 2: down, 3: left
 
     switch (randDirection) {
@@ -26,11 +66,11 @@ void Ghost::randomMove()
             setPos(x(), y() - 10);
         break;
     case 1: // Right
-        if (x() + rect().width() < sceneLength)
+        if (x() + 36 < sceneLength)
             setPos(x() + 10, y());
         break;
     case 2: // Down
-        if (y() + rect().height() < sceneWidth)
+        if (y() + 36 < sceneWidth)
             setPos(x(), y() + 10);
         break;
     case 3: // Left
@@ -40,5 +80,8 @@ void Ghost::randomMove()
     default:
         break;
     }
-}
 
+
+
+
+}
