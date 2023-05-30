28ba57e953c5f405245d8c8f06fd9fdce7e029ca
Beyza Karakurt <125734176+KarakurtBeyza@users.noreply.github.com>
2023-05-30 15:04:20 +0300

    Update Ghost.h

--- Ghost.h
+++ Ghost.h
@@ -6,20 +6,31 @@
 #include <QGraphicsRectItem>
 #include <QObject>
 #include "Player.h"
+#include <QGraphicsPixmapItem>
 
-class Ghost : public QObject, public QGraphicsRectItem
+class Ghost : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 private:
     int sceneLength;
     int sceneWidth;
-    Player& player;
+    int posX;
+    int posY;
+    bool isActive;
+    Player *player;
 
 public:
     Ghost(int sceneLength, int sceneWidth, Player& player);
+    int getPosX();
+    int getPosY();
+    bool getIsActive();
+    void setIsActive(bool control);
+    bool active();
+
 
 public slots:
     void randomMove();
 };
 
 #endif // GHOST_H
+
