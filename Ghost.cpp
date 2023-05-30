83e157decaad08a37378ea49adced098c4f983bb
Sumeyye05 <130572281+Sumeyye05@users.noreply.github.com>
2023-05-30 13:36:43 +0300

    Update Ghost.cpp

--- Ghost.cpp
+++ Ghost.cpp
@@ -1,104 +1,44 @@
+// Ghost.cpp
+
 #include "Ghost.h"
-#include "qpen.h"
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include <stdlib.h>
 #include "Player.h"
-#include <QtCore>
-#include <QGraphicsEllipseItem>
-#include <QGraphicsPixmapItem>
 
-Ghost::Ghost(int sceneLengthY , int sceneWidthX, Player &player ){
-
-    //setRect(10,10,10,10);
-    //setPen(QPen(QColor(255, 0, 0), 2));
-    //setBrush(QBrush(QColor(15, 144, 2)));
-    setPixmap(QPixmap(":/images/ghost.png"));
-    this->isActive = true;
-    this->player = &player;
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    std::uniform_int_distribution<int> distribution(0, sceneWidthX);
-    int posX = distribution(gen);
-    std::uniform_int_distribution<int> dis(0, sceneLengthY);
-    int posY = dis(gen);
-    setPos(posX,  posY);
-
-    this->posX = posX;
-    this->posY = posY;
+Ghost::Ghost(int sceneLength, int sceneWidth, Player& player)
+    : sceneLength(sceneLength), sceneWidth(sceneWidth), player(player)
+{
+    setRect(0, 0, 20, 20);
+    QTimer* timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(randomMove()));
+    timer->start(100);
 }
 
+void Ghost::randomMove()
+{
+    int randDirection = rand() % 4; // 0: up, 1: right, 2: down, 3: left
 
-int Ghost::getPosX() {
-    return posX;
-}
-
-int Ghost::getPosY() {
-    return posY;
-}
-
-bool Ghost::getIsActive(){
-    return (bool) this->isActive;
-}
-void Ghost::setIsActive(bool control){
-    this->isActive = control;
-}
-
-bool Ghost::active(){
-    bool isActive = true;
-
-    //hayaletin durma algoritmasi
-    if (player->getPosX() == this->getPosX()
-        ||  player->getPosY() == this->getPosY())
-    {
-        isActive = false;
-    }
-
-    return isActive;
-}
-
-
-void Ghost::randomMove(){
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distribution(0, 3);
-
-    bool boolen = true ;
-
-    while(boolen){
-
-        if(!this->active()){
-            boolen = false;
-        }
-
-        int randomNumber = distribution(gen);
-
-        switch(randomNumber)
-        {
-        case 0:
-            if (x()<=10 || y()<=10 )
-                break;
-            setPos(x()-10,y()-10);
-            break;
-        case 1:
-            if (x()>=490  || y()<=10 )
-                break;
-            setPos(x()+10,y()-10);
-            break;
-        case 2:
-            if (x()>=490  || y() >= 490 )
-                break;
-            setPos(x()+10,y()+10);
-            break;
-        case 3:
-            if (x()<=10 || y() >= 490 )
-                break;
-            setPos(x()-10,y()+10);
-            break;
-        }
-        QThread::msleep(100);
-
-        posX = x();
-        posY = y();
-
+    switch (randDirection) {
+    case 0: // Up
+        if (y() > 0)
+            setPos(x(), y() - 10);
+        break;
+    case 1: // Right
+        if (x() + rect().width() < sceneLength)
+            setPos(x() + 10, y());
+        break;
+    case 2: // Down
+        if (y() + rect().height() < sceneWidth)
+            setPos(x(), y() + 10);
+        break;
+    case 3: // Left
+        if (x() > 0)
+            setPos(x() - 10, y());
+        break;
+    default:
+        break;
     }
 }
+
