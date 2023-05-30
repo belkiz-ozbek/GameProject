53724e4fa81e00a7c6f92d55c00bf7613f14742d
ErvaYlmz <130571315+ErvaYlmz@users.noreply.github.com>
2023-05-30 23:27:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,30 +17,30 @@ int main(int argc, char* argv[])
     QGraphicsScene scene;
     scene.setSceneRect(0, 0, 500, 500);
     scene.setBackgroundBrush(QBrush(QColor(0,0,0)));
-    
+
     Player player1(40, 40, 0, 0, scene.sceneRect().width(), scene.sceneRect().height(), true);
-    
+
     player1.setFlag(QGraphicsItem::ItemIsFocusable);
     player1.setFocus();
     scene.addItem(&player1);
 
     Player player2(40, 40, 460, 460, scene.sceneRect().width(), scene.sceneRect().height(), false);
-    
+
     player2.setFlag(QGraphicsItem::ItemIsFocusable);
     player2.setFocus();
     scene.addItem(&player2);
 
     Ghost ghosts[10] = {
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1),
-        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1)
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2),
+        Ghost(scene.sceneRect().width(), scene.sceneRect().height(), player1, player2)
     };
 
     for (int i = 0; i < 10; i++) {
